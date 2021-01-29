b9c45903744c8db5253801da0519275089cae80f
Hajdu Marcell Ferenc <hajdulord@gmail.com>
2021-01-29 23:31:11 +0100

    Some minor changes

--- Assets/Scripts/Blocade.cs
+++ Assets/Scripts/Blocade.cs
@@ -8,11 +8,14 @@ namespace HMF
     {
         [SerializeField] private GameObject _blocade = null;
         [SerializeField] private GameObject _audio = null;
+
+        [SerializeField] private GameObject _audio2 = null;
         private void OnTriggerEnter2D(Collider2D other) {
             if (other.gameObject.layer == 9)
             {
                 _blocade.SetActive(true);
                 _audio.SetActive(true);
+                _audio2.SetActive(false);
             }
             
         }
@@ -21,6 +24,7 @@ namespace HMF
         {
             _blocade.SetActive(false);
             _audio.SetActive(false);
+            _audio2.SetActive(true);
         }
     }
 }
