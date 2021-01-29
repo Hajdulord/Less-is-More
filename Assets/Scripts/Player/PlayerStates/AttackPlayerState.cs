19b865c50d3ccfb0ac9e1a1682e9fd6187609acd
Hajdu Marcell Ferenc <hajdulord@gmail.com>
2021-01-29 01:22:37 +0100

    Finished implementing player attack

--- Assets/Scripts/Player/PlayerStates/AttackPlayerState.cs
+++ Assets/Scripts/Player/PlayerStates/AttackPlayerState.cs
@@ -1,28 +1,40 @@
 using System;
 using HMF.HMFUtilities.DesignPatterns.StatePattern;
+using UnityEngine;
 
 namespace HMF.Player.PlayerStates
 {
     public class AttackPlayerState : IState
     {
+        private PlayerController _player;
+        private Collider2D _collider;
+        private Animator _animator;
+        
+
+        public AttackPlayerState(PlayerController player, Animator animator)
+        {
+            _animator = animator;
+            _player = player;
+        }
+
         public void OnEnter()
         {
             //Start Attack Animation
+            _animator.SetTrigger("Attacking");
+            _collider = Physics2D.OverlapCircle(_player.attackPoint.position, _player.attackRange, _player.enemyLayers);
         }
 
-        public void OnExit()
+        public void OnExit() 
         {
-            //Start Attack Animation
+            _collider = null;
+            _player.Attacked = false;
         }
 
         public void Tick()
         {
-            Attack();
-        }
+            if (_collider == null) return;
 
-        private void Attack()
-        {
-            throw new NotImplementedException();
+            Debug.Log(_collider.name);
         }
     }
 }
